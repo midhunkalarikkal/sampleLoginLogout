0fca71ae28029bab849df5c7bf3cef8980dcc261
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-12-24 02:23:16 +0530

    UPdated app.js cookies

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
-      secure: process.env.NODE_ENV === 'production', 
+      secure: false,
       httpOnly: true,
     },
   })
